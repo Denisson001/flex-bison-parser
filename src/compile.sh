29f5c174d04b7b163f808ac72cedebfea6b749c6
Denisson001 <shpakovskij.denis@yandex.ru>
2019-12-27 23:15:04 +0300

    Update compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -2,6 +2,6 @@
 
 bison -d  grammar.y
 lex lexer.lex
-g++-7 -fsanitize=address -std=c++17 grammar.tab.c lex.yy.c \
+g++ -std=c++17 grammar.tab.c lex.yy.c \
 interpreter.cpp error_handler.cpp operation.cpp operator.cpp \
 expression.cpp dictionary.cpp variable.cpp main.cpp -o main
